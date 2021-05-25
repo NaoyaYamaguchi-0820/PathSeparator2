13de0440d3941850c04ca52e5c24fb2e5d00294e
NaoyaYamaguchi0820 <naoya.yamaguchi.mail@gmail.com>
2021-05-25 21:28:20 +0900

    プロジェクト ファイルを追加します。

---
+++ PathSeparator2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+
+namespace PathSeparator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string homePath = Directory.GetCurrentDirectory();
+            string inputFilePath = Path.Combine(homePath, "input.txt");
+
+
+            List<string> rowList = new List<string>();
+
+            // テキストファイルを読み込む
+            using (var reader = new StreamReader(inputFilePath))
+            {
+
+                string row;
+
+                while((row = reader.ReadLine()) != null){
+                    rowList.Add(row);
+                }
+
+            }
+
+            string outputFilePath = Path.Combine(homePath, "output.txt");
+
+            using (var writer = new StreamWriter(outputFilePath, false, Encoding.GetEncoding("Shift_JIS")))
+            {
+                foreach (string str in rowList)
+                {
+                    string path = Directory.GetParent(str).ToString();
+                    string name = Path.GetFileName(str);
+
+                    writer.WriteLine(path + "," + name);
+                }
+            }
+
+            // 終了
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+    }
+}
